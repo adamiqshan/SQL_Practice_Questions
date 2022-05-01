cebdd3eb2f5e12d6689073a3e9f8eb764b2cb944
Adam Iqshan <omai.mohmed9@gmail.com>
2022-05-01 10:36:39 +0000

    SQL problem 1795

--- Rearrange Products Table.sql
+++ Rearrange Products Table.sql
@@ -1,3 +1,6 @@
+-- Leetcode SQL problem 1795
+-- https://leetcode.com/problems/rearrange-products-table/
+
 -- Write an SQL query to rearrange the Products table so that each row has (product_id, store, price). If a product is not available in a store, do not include a row with that product_id and store combination in the result table.
 
 -- Return the result table in any order.
